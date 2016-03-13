4cfad490c01be18b58135b3b74f94ff10c64a694
Alexandre Quessy <alexandre@quessy.net>
2016-03-13 17:13:36 -0400

    Arduino: read pressure and broadcast it via OSC

--- arduino/arduino_pressure/arduino_pressure.ino
+++ arduino/arduino_pressure/arduino_pressure.ino
@@ -1,4 +1,4 @@
-// Import required libraries
+  // Import required libraries
 #include <mem.h>
 #include <ESP8266WiFi.h>
 #include <WiFiUDP.h>
@@ -10,11 +10,12 @@
     const char* ssid = "Westside-FU124";
     const char* password = "Betterave$pasFine122";
     int valueSendi = 0;
-    float valueSendf = 0.0;
+    int lastValue = 0;
     boolean isUp = true;
-​
+    IPAddress ip;
+
 WiFiUDP Udp;
-const IPAddress outIp(192, 168, 0, 102);
+//const IPAddress outIp(192, 168, 0, 102);
 const unsigned int outPort = 31340;
     
     void setup(void)
@@ -32,34 +33,30 @@ const unsigned int outPort = 31340;
     Serial.println("WiFi connected");
     // Print the IP address
     Serial.println(WiFi.localIP());
+
+    ip = WiFi.localIP();
+    ip[3] = 255;
+
     }
     
    
     void loop() {
-  sendCount ++;
-  if (sendCount > 100000){
-​
-  
-  if (valueSendf >= 10.0){
-​
-    isUp = false;
-  }else if ( valueSendf <= 0){
-    isUp = true;
-  }
-​
-  if (isUp){
-  valueSendf = valueSendf + 0.001;
-  }else{
-    valueSendf = valueSendf - 0.001;
-  }
-​
-   
-  OSCMessage msg("/test/");
-  msg.add(valueSendf);
-  Udp.beginPacket(outIp, outPort);
+ 
+  valueSendi = analogRead(A0);
+
+   if (lastValue != valueSendi){
+
+  OSCMessage msg("/force/");
+  msg.add(valueSendi);
+  Udp.beginPacket(ip, outPort);
   msg.send(Udp);
   Udp.endPacket();
   msg.empty();
+
+   }
+
+   lastValue = valueSendi;
+   delay(30);
       
     }
-    }
+    
