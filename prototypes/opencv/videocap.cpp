62894d543dfbc136fe4eab554e6fbd0ad15efd6c
Alexandre Quessy <alexandre@quessy.net>
2016-03-15 14:28:13 -0400

    update videocap.cpp - we will start from that for the blob tracker

--- prototypes/opencv/videocap.cpp
+++ prototypes/opencv/videocap.cpp
@@ -1,29 +1,36 @@
 #include "opencv2/opencv.hpp"
+#include <iostream>
 
 using namespace cv;
 
+static const int VIDEO_DEVICE_ID = 1;
+
 int main(int, char**)
 {
-    VideoCapture cap(0); // open the default camera
+    cv::VideoCapture cap(VIDEO_DEVICE_ID);
     if (! cap.isOpened())  // check if we succeeded
     {
-        return -1;
+        return 1;
     }
-    Mat edges;
-    namedWindow("edges", 1);
+
+    cv::Mat edges;
+    cv::namedWindow("edges", 1);
+
     while (true)
     {
-        Mat frame;
+        cv::Mat frame;
         cap >> frame; // get a new frame from camera
-        cvtColor(frame, edges, CV_BGR2GRAY);
-        GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
-        Canny(edges, edges, 0, 30, 3);
-        imshow("edges", edges);
-        if(waitKey(30) >= 0)
+        cv::cvtColor(frame, edges, CV_BGR2GRAY);
+        cv::GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
+        // cv::Canny(edges, edges, 0, 30, 3);
+        cv::imshow("edges", edges);
+        if (cv::waitKey(30) >= 0)
         {
             break;
         }
+        // TODO: also stop if the window has been destroyed
     }
+    std::cout << "Done" << std::endl;
     // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
 }
