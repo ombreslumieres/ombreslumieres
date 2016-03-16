31d81696ab63631f749408ae63c659ea48e9a6c6
Alexandre Quessy <alexandre@quessy.net>
2016-03-16 12:38:11 -0400

    arduino_pressure.ino: can choose unicast or broadcast

--- arduino/arduino_pressure/arduino_pressure.ino
+++ arduino/arduino_pressure/arduino_pressure.ino
@@ -3,8 +3,15 @@
  * This code is for the ESP8266 wifi device.
  * @author Louis-Robert Bouchard
  * @date 2016-03-16
+ * 
+ * - Start the Arduino IDE and open the Preferences window.
+ * - Enter the following URL: http://arduino.esp8266.com/package_esp8266com_index.json 
+ *   into Additional Board Manager URLs field.
+ * - Open Boards Manager from Tools > Board menu and install the esp8266 platform.
+ * - Choose the Olimex MOD-WIFI-ESP8266(-DEV) board type
+ * - download zip from https://github.com/ameisso/OSCLib-for-ESP8266 and unzip it in ~/Documents/Arduino/librairies/
  */
-#include <mem.h>
+//#include <mem.h>
 #include <ESP8266WiFi.h>
 #include <WiFiUDP.h>
 #include <OSCMessage.h>
@@ -12,17 +19,17 @@
     
 // WiFi parameters
 long sendCount = 0;
-const char* ssid = "Westside-FU124";
-const char* password = "Betterave$pasFine122";
+const char* ssid = "Dark Night";
+const char* password = "bitcoin12333";
 int valueSendi = 0;
 int lastValue = 0;
 boolean isUp = true;
-IPAddress ip;
 
 WiFiUDP Udp;
-// const IPAddress outIp(192, 168, 0, 102);
+IPAddress outIp(192, 168, 1, 6); // send to which IP
 const unsigned int outPort = 31340;
-    
+const bool USE_BROADCAST = false; // if set to true will broadcast
+
 void setup(void)
 { 
     // Start Serial
@@ -38,21 +45,24 @@ void setup(void)
     Serial.println("WiFi connected");
     // Print the IP address
     Serial.println(WiFi.localIP());
-    // Broadcast IP is the same as our local IP,
-    // but we replace the last number by 255
-    // Example: 192.168.0.255
-    ip = WiFi.localIP();
-    ip[3] = 255;
 }
    
 void loop()
 {
+    if (USE_BROADCAST)
+    {
+        // Broadcast IP is the same as our local IP,
+        // but we replace the last number by 255
+        // Example: 192.168.0.255
+        outIp = WiFi.localIP();
+        outIp[3] = 255;
+    }
     valueSendi = analogRead(A0);
     if (lastValue != valueSendi)
     {
         OSCMessage msg("/force");
         msg.add(valueSendi);
-        Udp.beginPacket(ip, outPort);
+        Udp.beginPacket(outIp, outPort);
         msg.send(Udp);
         Udp.endPacket();
         msg.empty();
