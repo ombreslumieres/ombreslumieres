c0b5262f9536c62f99e7cbec90cf21e6440f4186
Alexandre Quessy <alexandre@quessy.net>
2016-04-05 10:42:41 -0400

    more color scripts

---
+++ scripts/sh_color_cyan.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+OSC_SEND_PORT=31340
+IDENTIFIER=default
+COLOR_R=0
+COLOR_G=255
+COLOR_B=255
+osc-send -p ${OSC_SEND_PORT} /color ,siii ${IDENTIFIER} \
+    ${COLOR_R} ${COLOR_G} ${COLOR_B}
